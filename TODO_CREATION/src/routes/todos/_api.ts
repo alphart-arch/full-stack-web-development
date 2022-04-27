987f69a953f95ffd85626b7a6af86070ebcf9c79
alphart-arch <alphart07@gmail.com>
2022-04-27 12:18:03 +0530

    commit

--- TODO_CREATION/src/routes/todos/_api.ts
+++ TODO_CREATION/src/routes/todos/_api.ts
@@ -1,26 +1,41 @@
 import type { Request } from "@sveltejs/kit";
+import PrismaClient from "$lib/prisma";
+import type { Prisma } from "@prisma/client";
+//let todos:Todo[]=[];
+const prisma = new PrismaClient();
 
-let todos:Todo[]=[];
-
-export const api = (request:Request , data?:Record<string , unknown >) => {
+export const api = async(request:Request , data?:Record<string , unknown >) => {
    let body={};
    let status=500;
     switch (request.method.toUppercase()) {
         case "GET":
-            body=todos;
+            body=await prisma.todo.findMany();
             status=200;
             break;
         case "POST":
-            todos.push(data as Todo);
-            body=data;
+            body =await prisma.todo.create({
+                data:{
+                    created_at:data.created_at as Date,
+                    done:data.done as boolean,
+                    text:data.text as string
+                }
+            });
+            
+        //todos.push(data as Todo);
+            //body=data;
             status=201;
             break; 
         case "DELETE":
-            todos=todos.filter(todo => todo.uid !== request.params.uid)
+            body = await prisma.todo.delete({
+                where:{
+                    uid:request.params.uid
+                }
+            })
+            //todos=todos.filter(todo => todo.uid !== request.params.uid)
             status=200;    
             break;
         case "PATCH":
-            todos=todos.map(todo =>{
+            /*todos=todos.map(todo =>{
                 if(todo.uid === request.params.uid){
                     todo.text=data.text as string;
                 }
@@ -29,9 +44,18 @@ export const api = (request:Request , data?:Record<string , unknown >) => {
                 }
                 return todo;
 
-            });
+            });*/
+            body = await prisma.todo.update({
+                where:{
+                    uid:request.params.uid
+                },
+                data:{
+                    done:data.done,
+                    text:data.text
+                }
+            })
             status=200;
-            body=todos.find(todo => todo.uid === request.params.uid);
+            //body=todos.find(todo => todo.uid === request.params.uid);
             break;
     
         default:
